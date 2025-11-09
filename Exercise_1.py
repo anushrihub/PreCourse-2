b35ceb83c60c73e7ffa25635deba4fb5ee00dea7
Anushri <sastikaranushri@gmail.com>
2025-11-09 14:21:56 -0500

    exercise-1 binary sort

--- Exercise_1.py
+++ Exercise_1.py
@@ -5,18 +5,37 @@
 # if present, else returns -1 
 def binarySearch(arr, l, r, x): 
   
-  #write your code here
+  # write your code here
+  # sorting the given array
+  arr.sort()
+
+  # finding the middle index
+  mid_index = int((l + r) / 2)
+  print("func called with", l,r)
+  # checking if middle is equal to given element
+  if x == arr[mid_index]:
+    return mid_index
   
+  # checking if the given element is grater than middle element
+  elif (x > arr[mid_index]):
+    print("x > mid_index")
+    return binarySearch(arr, mid_index+1, r, x)
     
+  # checking if the given element is less than middle element
+  elif (x < arr[mid_index]):
+    print("x < mid_index")
+    return binarySearch(arr,l ,mid_index-1, x)
+  
+  return -1
   
 # Test array 
-arr = [ 2, 3, 4, 10, 40 ] 
-x = 10
+arr = [ 2, 3, 4, 10, 40] 
+x = 3
   
 # Function call 
 result = binarySearch(arr, 0, len(arr)-1, x) 
-  
+
 if result != -1: 
-    print "Element is present at index % d" % result 
+    print("Element is present at index ", result)
 else: 
-    print "Element is not present in array"
+    print("Element is not present in array")
