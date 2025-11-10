24a914f959cdab92dba270d46db170dda3fd251f
Anushri <sastikaranushri@gmail.com>
2025-11-09 21:37:13 -0500

    Exercise_3 mid point of a singly linked list

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,26 +1,53 @@
+import math
 # Node class  
 class Node:  
   
     # Function to initialise the node object  
-    def __init__(self, data):  
+    def __init__(self, data = None, next = None):
+        self.data = data
+        self.next = next  
         
 class LinkedList: 
   
-    def __init__(self): 
-        
+    def __init__(self):
+        # initializing head with none
+        self.head = None
   
     def push(self, new_data): 
-        
-  
-    # Function to get the middle of  
-    # the linked list 
+        # creating an object
+        next_node = Node(new_data)
+        if self.head:
+            node = self.head
+            while node.next:
+                node = node.next
+            node.next = next_node
+        else:
+            self.head = next_node
+
+    # Function to get the middle of the linked list 
     def printMiddle(self): 
+        length = 0
+        if self.head:
+            node = self.head
+            while node.next:
+                length += 1
+                node = node.next
+            # find the mid_index of the linked list
+            mid_index = math.ceil(length / 2)
+            node = self.head
+            # interate over the found mid_index
+            for _index in range(mid_index):
+                node = node.next
+            return node.data
+        return None
+        
 
 # Driver code 
 list1 = LinkedList() 
+
 list1.push(5) 
 list1.push(4) 
 list1.push(2) 
 list1.push(3) 
-list1.push(1) 
-list1.printMiddle() 
+list1.push(1)
+print(list1.printMiddle())
